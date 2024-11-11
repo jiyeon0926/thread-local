738faf2c438aa29765873ec04c079055023fb509
Yeon <yeoni9094@naver.com>
2024-11-11 15:52:12 +0900

    feat: 프로젝트 생성
    스레드 로컬에 대해 알아보기 위해 프로젝트 생성

---
+++ src/test/java/thread/local/ThreadLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package thread.local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
